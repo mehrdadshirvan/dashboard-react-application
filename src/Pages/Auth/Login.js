37c9c6ad9fa15c32782caaab1bf74172e8dec95a
Mehrdad <mehrdadshirvan@gmail.com>
2022-01-29 22:11:29 +0330

    create project

---
+++ src/Pages/Auth/Login.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Login extends React.Component {
+
+    render() {
+        return (
+            <>
+                <div className={`container-fluid`}>
+                    <div className={`row`}>
+                        <div className={`col-sm-6 m-auto`}>
+                            <div className={`card`}>
+                                <div className={`card-body`}>
+                                    <p>
+                                        Login
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
---
+++ src/Pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <>
+                <div className={`container-fluid`}>
+                    <div className={`row`}>
+                        <div className={`col-sm-6 m-auto`}>
+                            <div className={`card`}>
+                                <div className={`card-body`}>
+                                    <p>
+                                        Home
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
